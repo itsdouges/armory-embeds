d41a745778cf85c8e29a51c9de50d06b5d74be4c
madou <laheen@gmail.com>
2017-10-09 23:53:21 +1100

    chore(specializations): convert specs to use gw2 component

--- src/components/Specializations/index.js
+++ src/components/Specializations/index.js
@@ -1,23 +1,9 @@
 // @flow
 
-import type { Specializations, Traits } from 'armory-component-ui';
-
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import { Specialization, actions } from 'armory-component-ui';
-
-function mapStateToProps (state) {
-  return {
-    specializations: state.specializations,
-    traits: state.traits,
-  };
-}
+import React from 'react';
+import { Gw2Specialization } from 'armory-component-ui';
 
 type Props = {
-  specializations?: Specializations,
-  traits?: Traits,
-  fetchSpecializations?: (ids: Array<number>) => void,
   specs: Array<{
     id: number,
     traits?: Array<number>,
@@ -25,33 +11,16 @@ type Props = {
   className?: string,
 };
 
-export default connect(mapStateToProps, {
-  fetchSpecializations: actions.fetchSpecializations,
-})(
-class SkillsEmbed extends Component<Props> {
-  props: Props;
-
-  componentWillMount () {
-    const { specs, fetchSpecializations } = this.props;
-
-    fetchSpecializations && fetchSpecializations(specs.map((spec) => spec.id));
-  }
-
-  render () {
-    const { specs, specializations = {}, traits = {}, className } = this.props;
-
-    return (
-      <div className={className}>
-        {specs.map((spec) => (
-          <Specialization
-            data={spec}
-            key={spec.id}
-            specializations={specializations}
-            traits={traits}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const SpecializationsEmbed = ({ specs, className }: Props) => (
+  <div className={className}>
+    {specs.map((spec) => (
+      <Gw2Specialization
+        key={spec.id}
+        id={spec.id}
+        activeTraits={spec.traits}
+      />
+    ))}
+  </div>
 );
+
+export default SpecializationsEmbed;
